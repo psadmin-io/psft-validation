9bacb775284f519905c1af0ac1769ffad3796187
Kyle Benson <kyle@psadmin.io>
2022-05-13 13:22:25 -0500

    Update cli-example.sh

--- lib/cli-example.sh
+++ lib/cli-example.sh
@@ -1,10 +1,10 @@
-﻿#! /bin/sh
+#!/bin/sh
 
 # vars
 jmeter_home="/jmeter/5.4.1"
 jmeter_proj="/jmeter-main" # just to is relative path from this script location?
 
-# $prop_file    = "user.properties"
+prop_file="${jmeter_proj}/data/env.properties"
 plan_file="${jmeter_proj}/plans/pt-validation.jmx" 
 results_file="${jmeter_proj}/temp/pt-validation-results.$(date +"%Y-%b-%d-%H:%M:%S").jtl" # datetime stamp?
 run_log="${jmeter_proj}/temp/pt-validation.log"
@@ -13,6 +13,9 @@ export JAVA_OPTS="-Xms1024m -Xmx1024m"
 #$jmeter_home\bin\jmeter -p "$prop_file" -n -t "$plan_file" `
 cd $jmeter_home/bin || return
 ./jmeter -n -t $plan_file \
+  -p $prop_file \
+  -JOPRID="$User" \
+  -JPASS="$Pass"
   --logfile       $results_file \
   --jmeterlogfile $run_log
 
